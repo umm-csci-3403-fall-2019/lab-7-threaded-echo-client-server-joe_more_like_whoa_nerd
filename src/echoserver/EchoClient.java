e449f564ddad48f6def966af9ae4f320a231d3be
nkfoss <nathankfoss@gmail.com>
2019-10-31 11:20:20 -0500

    Add code from previous echo-client server lab

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,23 +1,67 @@
 package echoserver;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.DataInputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 
 public class EchoClient {
-	public static final int PORT_NUMBER = 6013;
 
-	public static void main(String[] args) throws IOException {
-		EchoClient client = new EchoClient();
-		client.start();
-	}
+    public static final int portNumber = 6013;
 
-	private void start() throws IOException {
-		Socket socket = new Socket("localhost", PORT_NUMBER);
-		InputStream socketInputStream = socket.getInputStream();
-		OutputStream socketOutputStream = socket.getOutputStream();
+    public static void main(String[] args) {
 
-		// Put your code here.
-	}
+        String server;
+
+        // Use "127.0.0.1", i.e., localhost, if no server is specified.
+        if (args.length == 0) {
+            server = "127.0.0.1";
+        } else {
+            server = args[0];
+        }
+
+        // Be prepared to handle any general exception
+        try {
+            // Establish connection with the server on the specified portnumber
+            Socket echoSocket = new Socket(server, portNumber);
+
+            // Grab the input/output streams so we can write/read directly to/from them
+            OutputStream os = echoSocket.getOutputStream();
+            DataInputStream is = new DataInputStream(echoSocket.getInputStream());
+
+            // We are communicating via bytes. Characters entered into the console are translated to bytes
+            // and sent to the server, which interprets them as bytes.
+            int c;
+            int responseByte;
+
+            // While there is a valid input to be received from System.in...
+            while ( (c = System.in.read()) != -1) {
+
+                // Write it and send it all together, immediately
+                os.write(c);
+                os.flush();
+
+                // Read the response and response and output it all immediately.
+                responseByte = is.read();
+                System.out.write(responseByte);
+                System.out.flush();
+            }
+
+            // Feedback for the user. This is commented out so the tests pass.
+//            System.out.println("Time to close.");
+
+            // Close everything when we are done.
+            os.close();
+            is.close();
+            echoSocket.close();
+        }
+
+        // Very minimal error handling.
+        catch (Exception e) {
+            System.err.println("Exception:  " + e);
+        }
+    }
+
 }
+
+
+// model: https://introcs.cs.princeton.edu/java/84network/EchoClient.java.html
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,27 +1,57 @@
 package echoserver;
 
+import java.io.DataInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
-	public static final int PORT_NUMBER = 6013;
 
-	public static void main(String[] args) throws IOException, InterruptedException {
-		EchoServer server = new EchoServer();
-		server.start();
-	}
+    public static final int portNumber = 6013;
 
-	private void start() throws IOException, InterruptedException {
-		ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
-		while (true) {
-			Socket socket = serverSocket.accept();
-			InputStream inputStream = socket.getInputStream();
-			OutputStream outputStream = socket.getOutputStream();
+    public static void main(String[] args) {
+        try {
+            // Start listening on the specified port
+            ServerSocket serverSocket = new ServerSocket(portNumber);
 
-			// Put your code here.
-		}
-	}
+            // The client is sending bytes, represented as an int
+            int c;
+
+            // Run forever, which is common for server style services
+            while (true) {
+
+                // Wait until someone connects, and indicate this to the user.
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Accepted connection from client.");
+
+                // Grab the input/output streams so we can write/read directly to/from them
+                OutputStream os = clientSocket.getOutputStream();
+                DataInputStream is = new DataInputStream(clientSocket.getInputStream());
+
+                // While there is valid input to be read...
+                while ( (c = is.read()) != -1) {
+
+                    // Write that input back into the output stream, and send it all immediately.
+                    os.write(c);
+                    os.flush();
+                }
+
+                // Close the streams/client socket since we're done.
+                System.out.println("Closing client connection");
+                os.close();
+                is.close();
+                clientSocket.close();
+            }
+
+        }
+
+        // Very minimal error handling.
+        catch (IOException ioe) {
+            System.out.println("We caught an unexpected exception");
+            System.err.println(ioe);
+        }
+    }
 }
+
+// model: https://introcs.cs.princeton.edu/java/84network/EchoServer.java.html
